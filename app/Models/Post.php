b57de6d5e43864fc41636bf4d658220cf729c03a
RenaldyNaufalTA <aldyreyn5@gmail.com>
2024-05-24 18:07:57 +0700

    Eloquent Relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
-    // protected $fillable = [
-    //     'title', 'author,', 'slug',
-    //     'body'
-    // ];
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
