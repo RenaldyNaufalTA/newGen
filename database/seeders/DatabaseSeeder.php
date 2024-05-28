bf2a3fe5aba43268bfdf4b7ed3ef21e15cf07158
RenaldyNaufalTA <aldyreyn5@gmail.com>
2024-05-28 11:14:31 +0700

    add posts category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,25 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //Membuat post dengan User secara bersamaan
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        Post::create([
-            'title' => 'Judul Artikel 1',
-            'author' => 'Ryou',
-            'slug' => 'judul-artikel-1',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eius itaque aut animi temporibus ducimus, debitis cumque non fuga repellendus magni nemo. Velit dolor nisi quas nobis fuga similique unde expedita accusamus omnis, odit eaque ipsam nulla officia modi animi tempore, repellat harum? Nesciunt repudiandae saepe impedit iure. Magnam ipsum reprehenderit et reiciendis iste quaerat, accusantium ducimus ratione explicabo repellendus? Voluptatum, ratione! Expedita distinctio id debitis eligendi cupiditate neque ea a explicabo? Ab tempore repellat, corporis numquam exercitationem ducimus ipsam consequuntur, obcaecati expedita voluptates est officia nobis delectus odio officiis eum culpa reiciendis? Expedita autem obcaecati non, ex ducimus voluptas!'
-        ]);
-
-        Post::create([
-            'title' => 'Judul Artikel 2',
-            'author' => 'Ren',
-            'slug' => 'judul-artikel-2',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ipsum fugiat quisquam est eaque ratione distinctio quidem magni laudantium voluptas corporis rem minus ex, officiis qui maxime quia fuga hic?'
-        ]);
+        Post::factory(100)->recycle([Category::factory(3)->create(), User::factory(5)->create()])->create();
     }
 }
